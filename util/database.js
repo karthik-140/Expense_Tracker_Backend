4047bff52a3fd6980e8c6de7814935ec701cb8b5
karthik-140 <shanigaramkarthik@gmail.com>
2024-07-03 14:23:48 +0530

    added dialectModule

--- util/database.js
+++ util/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_ROOT_PASSWORD, {
   dialect: 'mysql',
-  host: process.env.DATABASE_HOST
+  host: process.env.DATABASE_HOST,
+  dialectModule: require('mysql2'),
 })
 
 module.exports = sequelize;
